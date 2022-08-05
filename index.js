f2b934425df4b24371a6bc8a8f474bc7923995e7
Coltonb-boop <coltonbitz@gmail.com>
2022-08-05 15:45:52 -0600

    Finished adding both missing features. Got a little confused with the last implementation and why it was returning true

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ class Transaction {
   }
 
   commit() {
+    if (!this.isAllowed()) return false;
     this.time = new Date();
     this.account.addTransaction(this);
+    return true;
   }
 
 }
@@ -37,12 +39,20 @@ class Withdrawal extends Transaction {
     return -this.amount;
   }
 
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
 }
 
 class Deposit extends Transaction {
 
   get value() {
     return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
@@ -59,12 +69,12 @@ t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
-
 t3 = new Deposit(100, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
+
+t2 = new Withdrawal(9.99, myAccount);
+t2.commit();
+console.log('Transaction 2:', t2);
 
 console.log('Balance:', myAccount.balance);
